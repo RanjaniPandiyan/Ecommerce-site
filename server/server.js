9c66afd17dd3a4e81e2b47cdc111586ea15efff4
RanjaniPandiyan <ranjanipandiyan2@gmail.com>
2026-05-25 16:19:32 +0530

    fix: update CORS origin to allow live Vercel frontend

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ecommerce-nu-liard.vercel.app"],
     credentials: true,
   }),
 );
