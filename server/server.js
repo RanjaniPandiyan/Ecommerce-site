3536d208ad7679b3f8da6492a1a5037fa868b8bb
RanjaniPandiyan <ranjanipandiyan2@gmail.com>
2026-05-25 20:42:11 +0530

    fix the CORS error

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://dev-ranjani.vercel.app"],
-    credentials: true,
+    withCredentials: true,
   }),
 );
 app.use(express.json());
