d0520b6c4a379e011e801691992c901278b1f846
RanjaniPandiyan <ranjanipandiyan2@gmail.com>
2026-05-28 15:21:55 +0530

    fix:Cors error Solved for Client

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://dev-ranjani.vercel.app",
+      "https://shopysite.vercel.app/",
     ],
     credentials: true,
   }),
