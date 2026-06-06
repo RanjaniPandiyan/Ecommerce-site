332f79819b3f97558fd9099d87ca4cac6591c1cd
RanjaniPandiyan <ranjanipandiyan2@gmail.com>
2026-06-06 14:16:21 +0530

    Fix:Server issues

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const categoryRoutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 const app = express();
 app.set("trust proxy", 1);
 // connect DB
@@ -35,7 +36,7 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/user", userRoutes);
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // server start
 app.listen(5000, () => {
   console.log("Server running on port 5000");
