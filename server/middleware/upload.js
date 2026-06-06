534d5fd9e553fd8212bd273da396649cb72fcf20
RanjaniPandiyan <ranjanipandiyan2@gmail.com>
2026-06-06 14:10:23 +0530

    Fix:Admin Panel Products Page

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -3,14 +3,13 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // folder
+    cb(null, path.join(__dirname, "../uploads"));
   },
+
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + path.extname(file.originalname);
     cb(null, uniqueName);
   },
 });
 
-const upload = multer({ storage });
-
-module.exports = upload;
+module.exports = multer({ storage });
